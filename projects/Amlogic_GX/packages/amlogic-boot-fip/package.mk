2a4ad6c6966e4dc8b32490def64a3f20820918f2
chewitt <github@chrishewitt.net>
2018-03-04 09:54:33 +0000

    amlogic: reuse Odroid_C2 device name

--- projects/Amlogic_GX/packages/amlogic-boot-fip/package.mk
+++ projects/Amlogic_GX/packages/amlogic-boot-fip/package.mk
@@ -44,7 +44,7 @@ case "$DEVICE" in
     PKG_URL="https://github.com/BayLibre/u-boot/releases/download/v2017.11-libretech-cc/khadas-vim_fip_${PKG_VERSION}.tar.gz"
     PKG_SOURCE_DIR="fip"
     ;;
-  "OdroidC2")
+  "Odroid_C2")
     PKG_VERSION="s905_6.0.1_v3.7"
     PKG_SHA256="3ee700fd3a6439997060ac6d21217b0adba3a801876707fae70988f8ce6c3fef"
     PKG_URL="https://github.com/hardkernel/u-boot/archive/${PKG_VERSION}.tar.gz"
