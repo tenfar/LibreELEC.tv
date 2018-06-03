f931267eb1e1e38b450fa5cca0371bcacfddd2fa
Jonas Karlman <jonas@kwiboo.se>
2018-06-03 11:53:58 +0200

    linux: update rockchip-4.4 to 219b8ca8

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -50,8 +50,8 @@ case "$LINUX" in
     PKG_BUILD_PERF="no"
     ;;
   rockchip-4.4)
-    PKG_VERSION="eae92ae2b930999857df47c3057327c1c490454b"
-    PKG_SHA256="da453ca6ecefc3719a1165bc7b08fe00fc2b50ab64f6289ef6f3670a9fc1ceca"
+    PKG_VERSION="219b8ca8ff3e28d5a1da7d68af2defcbeec53562"
+    PKG_SHA256="54078c490480b6306aa228be7e775112746f1ec7e30d003b831344fbd386acad"
     PKG_URL="https://github.com/rockchip-linux/kernel/archive/$PKG_VERSION.tar.gz"
     PKG_SOURCE_DIR="kernel-$PKG_VERSION"
     ;;
