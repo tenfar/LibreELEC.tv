8f04d44e38a19489f843d8d47f7a550b50d41ec9
Jonas Karlman <jonas@kwiboo.se>
2018-06-03 11:53:58 +0200

    rkbin: update to 784b3ef

--- packages/tools/rkbin/package.mk
+++ packages/tools/rkbin/package.mk
@@ -17,8 +17,8 @@
 ################################################################################
 
 PKG_NAME="rkbin"
-PKG_VERSION="f64ded6"
-PKG_SHA256="7b858ac964058da83cd96314184d2c5f834a9b2cc6b805be424a661fd9836b54"
+PKG_VERSION="784b3ef"
+PKG_SHA256="5cce2634df647bdb4b4eb0921fb1e52d5bd2c9e1e0b4c5d2a0593f1fe6dc72bb"
 PKG_ARCH="arm aarch64"
 PKG_LICENSE="nonfree"
 PKG_SITE="https://github.com/rockchip-linux/rkbin"
